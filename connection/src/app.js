f3ba4551c9f53ff41146ed12287ddc73197580ce
danielorozco14 <00200617@uca.edu.sv>
2018-11-06 08:01:19 -0600

    Configurado app.js e index.pug

--- connection/src/app.js
+++ connection/src/app.js
@@ -10,12 +10,12 @@ app.set('port',process.env.PORT || 3001);
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname,'views'));
 app.set('view engine','pug');
+app.set('views', path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname, "public")));
 
 //rutas
 app.use('/',require('./routes/routes'));
-
 app.listen(app.get('port'),()=>{
-    console.log(`Server on listen on port : ${app.get('port')}`);
+    console.log(`Server listening on port : ${app.get('port')}`);
 });
