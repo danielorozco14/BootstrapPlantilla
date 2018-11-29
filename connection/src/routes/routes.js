d6bcac0adc2e54eaa01c4bd47e81c40783155586
Tania Orellana <00123717@uca.edu.sv>
2018-11-29 09:19:29 -0600

    Agregar routers a archivo router. Falta modularizar routers y controladores anteriores.

--- connection/src/routes/routes.js
+++ connection/src/routes/routes.js
@@ -2,9 +2,25 @@ const express = require('express');
 const router = express.Router();
 var packsController = require("../controllers/packsController");
 
+/** Modularizar controladores. */
+
+var indexRouter = require('./routes/index');
+var adminsRouter = require('./routes/admin');
+var juegosRouter = require('./routes/juegos');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
+var chatRouter = require('./routes/chat');
+
 router.get('/',packsController.saludo);
 router.get('/login',packsController.login);
 router.get('/register',packsController.register);
 router.get('/chat',packsController.chat);
+
+app.use('/', indexRouter);
+app.use('/admin', adminsRouter);
+app.use('/juegos', juegosRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/chat', chatRouter);
 
 module.exports = router;
